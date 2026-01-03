0a1cd051317d19052b5078d1d141172363cc40c7
Tahim Bhuiya <tbhuiya2018@fau.edu>
2026-01-03 17:17:26 -0500

    Added onto primMST function

--- prims.cpp
+++ prims.cpp
@@ -38,7 +38,7 @@ void primMST(int V, vector<vector<pii>>& graph) {
 
         // Traverse all adjacent vertices
         for (auto edge : graph[u]) {
-               
+            int v = edge.first;      
             
 
             
