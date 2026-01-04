20da6875d16b1aff3bfd1b75e476ac50d3da7637
Tahim Bhuiya <tbhuiya2018@fau.edu>
2026-01-04 17:23:54 -0500

    Added onto primMST function

--- prims.cpp
+++ prims.cpp
@@ -53,5 +53,7 @@ void primMST(int V, vector<vector<pii>>& graph) {
     // Print the MST
     cout << "Edges in the Minimum Spanning Tree:\n";
     cout << "Edge \tWeight\n";
-    
+    for (int i = 1; i < V; i++) {
+        
+    }
 }
